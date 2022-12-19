8353323f1f0c66fd4aa4370d59873d3f453d68ba
thaonhunguyen <nguyenthaonhu2610@gmail.com>
2022-12-19 16:46:21 +0000

    Modify functions for object detection

--- object_detection/object_detection.py
+++ object_detection/object_detection.py
@@ -19,34 +19,36 @@ parser.add_argument('--dataset_name', '-d', type=str, default='V3C', help='Input
 
 def main(args):
     DATASET_NAME = args.dataset_name
+    print("Dataset name: ", DATASET_NAME)
+    # clip = CLIPSearchEngine(src_path=DATASET_MASTER_PATH, feature_path=args.feature_path, batch_size=args.batch_size, generate_features=args.generate_features)
     if DATASET_NAME == 'marine':
-        dataset_path = osp.join(DISK_PATH, VBS_PATH, MARINE_PATH, MARINE_VIDEO_PATH)
+        dataset_path = osp.join(MARINE_PATH, MARINE_VIDEO_PATH)
     elif DATASET_NAME == 'V3C':
-        dataset_path = osp.join(DISK_PATH, VBS_PATH, V3C_KEYFRAME_DATA_PATH, 'keyframes')
-    image_name_path = osp.join(dataset_path, f'{DATASET_NAME}_filenames.txt')
+        dataset_path = osp.join(V3C_KEYFRAME_DATA_PATH, 'keyframes')
+    src_path = osp.join(DISK_PATH, VBS_PATH)
+    image_name_path = osp.join(src_path, f'{DATASET_NAME}_filenames.txt')
     
-    src_path=osp.join(DISK_PATH, VBS_PATH)
-    # print(src_path)
     data = dataset(dataset_name=DATASET_NAME, src_path=src_path, dataset_path=dataset_path, image_name_path=image_name_path)
     data.get_file_name()
+    # print(len(data.image_names))
 
     # Model
     model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5_models/yolov5m.pt')
 
-    for item in tqdm(data.image_names[:10]):
+    for item in tqdm(data.image_names):
         concepts = helpers.convert_to_concepts(item, dataset_name=DATASET_NAME)
-        print(concepts)
-        label_curr_path = osp.join(MASTER_PATH, 'VBS2023/VideoCLIP-VBS2023', 'object_detection/labels', concepts['video'])
-        image_curr_path = osp.join(MASTER_PATH, 'VBS2023/VideoCLIP-VBS2023', 'object_detection/images', concepts['video'])
+        # print(concepts)
+        label_curr_path = osp.join(MASTER_PATH, 'VBS2023/VideoCLIP-VBS2023', 'object_detection/labels', DATASET_NAME, concepts['video'])
+        image_curr_path = osp.join(MASTER_PATH, 'VBS2023/VideoCLIP-VBS2023', 'object_detection/images', DATASET_NAME, concepts['video'])
         detection = model(item) 
     #    temp = detection.save(image_curr_path)
         bbox = detection.pandas().xyxy[0]
-        # try:
-        if not os.path.exists(label_curr_path):
-            os.makedirs(label_curr_path)
-        helpers.save_df_to_json(bbox, osp.join(label_curr_path, '{0}.json'.format(concepts['filename'])))
-        # except:
-        #     print(item)
+        try:
+            if not os.path.exists(label_curr_path):
+                os.makedirs(label_curr_path)
+            helpers.save_df_to_json(bbox, osp.join(label_curr_path, '{0}.json'.format(concepts['filename'])))
+        except:
+            print(item)
         
 if __name__ == '__main__':
     args = parser.parse_args()
