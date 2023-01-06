09af08d631a3ef80d487ee937c67aa004e00efb0
thaonhunguyen <nguyenthaonhu2610@gmail.com>
2023-01-06 09:23:50 +0000

    modify functions for marine data

--- server.py
+++ server.py
@@ -8,13 +8,15 @@ import numpy as np
 import time
 from configs import *
 
-generate_features = True
+generate_features = False
 
-print("Dataset name: ", DATASET_NAME, file=sys.stdout)
-clip = CLIPSearchEngine(src_path=DATASET_MASTER_PATH, feature_path=FEATURE_PATH, generate_features=generate_features)
-clip.dataset.get_file_name()
-print('Loading Features', file=sys.stdout)
-clip.load_features()
+v3c_clip = CLIPSearchEngine('V3C', src_path=MASTER_PATH, feature_path=V3C_FEATURE_DICT_PATH, generate_features=generate_features, dataset_path=V3C_DATASET_PATH, image_name_path=V3C_IMAGE_NAME_PATH)
+v3c_clip.dataset.get_file_name()
+v3c_clip.load_features()
+
+marine_clip = CLIPSearchEngine('marine', src_path=MASTER_PATH, feature_path=MARINE_FEATURE_DICT_PATH, generate_features=generate_features, dataset_path=MARINE_DATASET_PATH, image_name_path=MARINE_IMAGE_NAME_PATH)
+marine_clip.dataset.get_file_name()
+marine_clip.load_features()
 
 # with open("feature_dict.pkl", "rb") as a_file:
 #     print('Loading Feature Dict', file=sys.stdout)
@@ -75,13 +77,17 @@ def search():
     body_value = get_from_body(body)
     query = body_value('query', '')
     ocr = body_value('ocr', '')
+    #dataset_name = body_value
     colors = body_value('colors', [])
     metas = body_value('metas', [])
-    dataset = body_value('dataset', 'V3C')
+    dataset = body_value('dataset', 'V3C') 
     total = body_value('total', 100)
-    best_images = clip.search_query(query, num_matches=total * 3, ss_type='other')
-
-    # print(best_images[:1])
+    if dataset == 'V3C':
+        best_images = v3c_clip.search_query(query, num_matches=total * 3, ss_type='other')
+    elif dataset == 'MARINE':
+        print('loading marine features', file=sys.stdout)
+        best_images = marine_clip.search_query(query, num_matches=total * 3, ss_type='other')
+        # print(best_images[:10], file=sys.stdout)
     return {
         # "result": list(map(format_result, best_images)),
         "result": group_result(best_images)
@@ -96,20 +102,34 @@ def servertime():
     }
 
 
-@app.route('/api/find_similar_keyframes/<video_id>/<keyframe_id>', methods=['GET'])
-def similar_keyframes(video_id, keyframe_id):
+@app.route('/api/find_similar_keyframes/<dataset>/<video_id>/<keyframe_id>', methods=['GET'])
+def similar_keyframes(dataset, video_id, keyframe_id):
     # TODO
-    img_query = f'shot{video_id}_{keyframe_id}_RKF.png'
-    feature = clip.feature_dict[img_query]
-    feature_vec = np.expand_dims(feature, axis=0)
-    feature_vector = feature_vec.astype('float32')
-    similarities = (feature_vector @ clip.features.T).squeeze(0)
-    indices = similarities.argsort()[-50:][::-1]
-    best_matched_image_names = [clip.dataset.image_names[item] for item in indices]
+    if dataset == 'V3C':
+        img_query = f'shot{video_id}_{keyframe_id}_RKF.png'
+        feature = v3c_clip.feature_dict[img_query]
+        feature_vec = np.expand_dims(feature, axis=0)
+        feature_vector = feature_vec.astype('float32')
+        similarities = (feature_vector @ v3c_clip.features.T).squeeze(0)
+        indices = similarities.argsort()[-50:][::-1]
+        best_matched_image_names = [v3c_clip.dataset.image_names[item] for item in indices]
 
-    result = [convert_to_concepts(item, dataset_name=clip.dataset_name) for item in best_matched_image_names]
+        result = [convert_to_concepts(item, dataset_name=v3c_clip.dataset_name) for item in best_matched_image_names]
+    
+    ## TO DO: change to marine dataset
+    elif dataset == 'MARINE':
+        
+        img_query = f'{video_id}_{keyframe_id}.jpg'
+        feature = marine_clip.feature_dict[img_query]
+        feature_vec = np.expand_dims(feature, axis=0)
+        feature_vector = feature_vec.astype('float32')
+        similarities = (feature_vector @ marine_clip.features.T).squeeze(0)
+        indices = similarities.argsort()[-50:][::-1]
+        best_matched_image_names = [marine_clip.dataset.image_names[item] for item in indices]
+
+        result = [convert_to_concepts(item, dataset_name=marine_clip.dataset_name) for item in best_matched_image_names]
     return {
-        "result": list(map(format_result, result[:1000])),
+        "result": group_result(list(map(format_result, result[:1000]))),
     }
     
     
